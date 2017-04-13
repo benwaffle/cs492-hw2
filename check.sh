9e8d6fdc4b1bd91144d68bbc9a96e61e4e734785
Ben Iofel <iofelben@gmail.com>
2017-04-13 16:50:38 -0400

    check.sh also checks page size = 32

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@
 ulimit -c 0
 for alg in FIFO LRU CLOCK; do
     for pp in + -; do
-        for ps in 1 2 4 8 16; do
+        for ps in 1 2 4 8 16 32; do
             echo "===================";
             echo ./main plist.txt ptrace.txt $ps $alg $pp;
             ./main plist.txt ptrace.txt $ps $alg $pp | tail -n1;
