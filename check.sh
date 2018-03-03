22eedc09079682f925196d0cd4dd2dde72281a35
Ben Iofel <iofelben@gmail.com>
2018-03-03 18:44:35 -0500

    update check.sh

--- check.sh
+++ check.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+function go {
+    ./main plist.txt ptrace.txt $1 $2 $3 | sed 's/.*: //'
+}
+
 ulimit -c 0
 for alg in FIFO LRU CLOCK; do
     for pp in + -; do
         for ps in 1 2 4 8 16 32; do
-            echo "===================";
-            echo ./main plist.txt ptrace.txt $ps $alg $pp;
-            ./main plist.txt ptrace.txt $ps $alg $pp | tail -n1;
+            #echo "===================";
+            #echo ./main plist.txt ptrace.txt $ps $alg $pp;
+            echo "$alg,$ps,$pp" $(go $ps $alg $pp);
         done
     done
 done
